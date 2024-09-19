42edcfda3e6fabae043978532cc15531909b0d2e
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-19 21:00:58 +0300

    feat: add exchange rates api

---
+++ src/libs/api/api.ts
@@ -0,0 +1,4 @@
+export {
+	type CurrencyRate,
+	getExchangeRates,
+} from "./exchange-rates/get-exchange-rates.api";
---
+++ src/libs/api/exchange-rates/get-exchange-rates.api.ts
@@ -0,0 +1,32 @@
+export interface CurrencyRate {
+	txt: string;
+	rate: number;
+	cc: string;
+}
+
+interface ExchangeRateApiResponse {
+	txt: string;
+	rate: number;
+	cc: string;
+	r030?: number;
+	exchangedate?: string;
+}
+
+export const getExchangeRates = async (): Promise<CurrencyRate[]> => {
+	const response = await fetch(
+		"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",
+	);
+	if (!response.ok) {
+		throw new Error("Failed to fetch exchange rates");
+	}
+
+	const data: ExchangeRateApiResponse[] = await response.json();
+	return [
+		{ txt: "гривна", rate: 1, cc: "UAH" },
+		...data.map((rate) => ({
+			txt: rate.txt,
+			rate: rate.rate,
+			cc: rate.cc,
+		})),
+	];
+};
