bb6cccddcde040fea0aa4dfdaed72f478139d537
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-20 19:55:46 +0300

    chore: update main

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ErrorBoundary>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</ErrorBoundary>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</StrictMode>,
 );
